6bd702e88cc78d46fc3c82e5e56c4ec35572ead6
Mamdouh Douidar <mamdouidar@gmail.com>
2022-11-24 08:39:01 +0200

    Add Security Configurations for Swagger

--- Pwynt.Api/Program.cs
+++ Pwynt.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Pwynt.Core.Helpers;
 using Pwynt.Core.Interfaces;
 using Pwynt.Core.Services;
@@ -17,7 +18,49 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFramework
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Pwynt",
+        Description = "Point of Sale API with asp.net core, entity framework, JWT authentication, Onion Architecture.",
+        TermsOfService = new Uri("https://github.com/Mamdouidar/Pwynt"),
+        Contact = new OpenApiContact
+        {
+            Name = "Mamdouh Douidar",
+            Email = "mamdouidar@gmail.com",
+            Url = new Uri("https://github.com/Mamdouidar")
+        },
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Please Type 'Bearer' then enter your JWT Key."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
 builder.Services.AddAuthentication(options =>
