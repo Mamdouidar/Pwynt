eb724c9393f5700c4c1102535428881872b34890
Mamdouh Douidar <mamdouidar@gmail.com>
2022-11-27 15:27:49 +0200

    Add SeedOrderItems Migration

---
+++ Pwynt.Data/Migrations/20221127132055_SeedOrderItems.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pwynt.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedOrderItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 1, 1, 25.99, 1, 1 }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 2, 2, 99.98, 5, 1 }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 3, 1, 959.00, 3, 2 }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 4, 1, 25.99, 1, 3 }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 5, 1, 1449.99, 4, 3 }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Id", "Quantity", "TotalAmount", "ProductId", "OrderId" },
+                values: new object[] { 6, 1, 199.99, 8, 4 }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [OrderItem]");
+        }
+    }
+}
